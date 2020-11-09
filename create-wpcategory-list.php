1872cfcd2ddd39d66a583b6340a5965f2bab852d
weeklyplugin <73271176+weeklyplugin@users.noreply.github.com>
2020-11-09 23:49:09 +0530

    Update create-wpcategory-list.php
    Add active class

--- create-wpcategory-list.php
+++ create-wpcategory-list.php
@@ -1,19 +1,26 @@
-  <section class="top-links">
-      <div class="container">
+
         <div class="desktop-top-blog-links">
         <ul class="sf-blog-toplinks-ul">
-        <li><a href="https://selectedfirms.co/blog">All</a></li>
+        <li><a href="<?php echo site_url();?>">All</a></li>
         <?php  $taxonomy = 'category';
  
+ $term_id = 0;
+    if(is_singular('post')){ // post type is optional.
+      $terms = wp_get_post_terms( $post->ID, $taxonomy, array("fields" => "ids") );
+      if(!empty($terms))
+        $term_id = $terms[0]; //we need only one term id
+    }
+
  echo wp_list_categories(
    array(
      'show_count'   => false,
    'pad_counts'   => false,
    'use_desc_for_title' => false,
    'title_li'=>'',
-   'hierarchical' =>false)
+   'hierarchical' =>false,
+   'current_category' => $term_id
+   )
  );
  ?>
  </ul>
-        
         </div>

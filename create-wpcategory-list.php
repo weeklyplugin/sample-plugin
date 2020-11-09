45771dd67f22ce25b08fb98d20f863726ec70ca7
weeklyplugin <73271176+weeklyplugin@users.noreply.github.com>
2020-11-10 00:33:45 +0530

    Update create-wpcategory-list.php
    Add select in wordpress

--- create-wpcategory-list.php
+++ create-wpcategory-list.php
@@ -1,26 +1,34 @@
-
+ <section class="top-links">
+      <div class="container">
         <div class="desktop-top-blog-links">
         <ul class="sf-blog-toplinks-ul">
         <li><a href="<?php echo site_url();?>">All</a></li>
         <?php  $taxonomy = 'category';
  
- $term_id = 0;
-    if(is_singular('post')){ // post type is optional.
-      $terms = wp_get_post_terms( $post->ID, $taxonomy, array("fields" => "ids") );
-      if(!empty($terms))
-        $term_id = $terms[0]; //we need only one term id
-    }
-
  echo wp_list_categories(
    array(
      'show_count'   => false,
    'pad_counts'   => false,
    'use_desc_for_title' => false,
    'title_li'=>'',
-   'hierarchical' =>false,
-   'current_category' => $term_id
-   )
+   'hierarchical' =>false)
  );
  ?>
  </ul>
+
+        
         </div>
+
+        <div class="mobile-top-blog-links" >
+        <?php wp_dropdown_categories(   array(
+
+'show_option_all'=>'All',
+  'show_count'   => false,
+  'class'            => 'myselect form-control form-control-sm'
+
+
+) ); ?>
+          
+        </div>
+      </div>
+    </section>
